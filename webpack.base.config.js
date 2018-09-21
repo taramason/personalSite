ed2b87c482c90d58901b5644a679172c765ea854
Tara Mason <tara.mason@gmail.com>
2018-09-21 10:35:12 -0600

    removed some dead code, cleaned up

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -28,10 +28,7 @@ module.exports = {
               modules: true,
               localIdentName: '[name]__[local]___[hash:base64:5]'
             }
-          },
-          // {
-          //   loader: 'sass-loader',
-          // }
+          }
         ])
       },
      {
