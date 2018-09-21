1ed87e335fe39fe939f135ae250265abeb51d08d
Tara Mason <tara.mason@gmail.com>
2018-09-21 09:04:46 -0600

    changed fonts, maybe this helped??

--- app/index.js
+++ app/index.js
@@ -6,15 +6,6 @@ var App = require('./components/App');
 require ('./styles/index.css');
 
 
-//what component needs:
-//UI (almost always)
-//state
-//lifecycle events
-
-
- //using a component class because IRL I'm assuming this page would (as opposed to a stateless functional component, which is what I wanted to use initially)
-
-
 ReactDOM.render (
   <App />,
   document.getElementById('app')
